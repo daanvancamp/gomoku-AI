b9d520f4fe042efdfcdaa46e0045daf841062b12
daanvancamp <163452511+daanvancamp@users.noreply.github.com>
2024-07-20 10:48:17 +0200

    The interface now uses ttk objects.

--- mainmenu.py
+++ mainmenu.py
@@ -2,6 +2,8 @@ import sys
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+from turtle import st
+from unittest.loader import VALID_MODULE_NAME
 import gomoku
 from ai import GomokuAI
 gomoku_ai=GomokuAI(15)#board_size
@@ -13,7 +15,7 @@ from lezen_stukken_en_trivia import TE_DETECTEREN_KLEUR, initialiseer_spelbord_j
 #todo: make it look nice
 
 WIDTH = 240 #origineel 230
-HEIGHT = 315
+HEIGHT = 315 #origineel 315
 game_instance = gomoku.GomokuGame(filereader.create_gomoku_game("consts.json"))
 
 root = Tk()
@@ -118,7 +120,14 @@ def start_new_game(is_training=False, moves:dict=None):
         if is_training:
             p1.set("MM-AI")
             set_player_type(0)
-        runs = int(game_runs.get())
+        valid_number = False
+        while not valid_number:
+            try:
+                runs = int(game_runs.get())
+                valid_number=True
+            except:
+                print("invalid number, try again")
+
         game_instance.ai_delay = delayvar.get()#waar of niet waar (boolean)
         stats.should_log = logvar.get()
         stats.setup_logging(p1.get(), p2.get())
@@ -128,8 +137,6 @@ def start_new_game(is_training=False, moves:dict=None):
                 initialiseer_spelbord_json_bestanden()#geen stukken op bord
             except:
                 raise Exception("Fout in functie: initialiseer_spelbord_json_bestanden")
-
-            print("in loop")
             
             stats.log_message(f"Game {i+1} begins.")
             game_instance.current_game = i+1
@@ -155,68 +162,80 @@ def game_over():
 def quit_game():
     sys.exit()#beeindig het programma volledig
 
+#style_numbers = ["georgia", 10, "white", 12, 2]#font, size, color, bold, underline (already defined)
+style=ttk.Style()
+style.configure("TButton", font=(style_numbers[0], style_numbers[1]),bg=style_numbers[2],ipadx=style_numbers[3],ipady=style_numbers[4],pady=15)#font=georgia, size=10;bg=white
+style.configure("TRadiobutton",fg="white",bg="green")
+style.configure("TEntry",fg="white",bg="green")
+style.configure("TLabel", font=(style_numbers[0], style_numbers[1]),fg="white",bg="green")#font=georgia, size=10
+style.configure("TCheckbutton", font=(style_numbers[0], style_numbers[1]),fg="white",bg="green")#font=georgia, size=10
 
 ### TABS ###
 ttk.Label(tab1)
-button_1 = Button(tab1, text="New Game", bg=style_numbers[2], font=(style_numbers[0], style_numbers[1]), width=style_numbers[3], height=style_numbers[4], command=lambda: start_new_game(False))
+button_1 = ttk.Button(tab1, text="New Game", command=lambda: start_new_game(False), style="TButton")
 button_1.grid(row=0, column=0, sticky="w")
-player1typelabel = Label(tab1, text="Player 1", font=(style_numbers[0], style_numbers[1]))
+player1typelabel = ttk.Label(tab1,style="TLabel", text="Player 1")
 player1typelabel.grid(row=2, column=0, sticky="w")
-player2typelabel = Label(tab1, text="Player 2", font=(style_numbers[0], style_numbers[1]))
+player2typelabel = ttk.Label(tab1, text="Player 2", style="TLabel")
 player2typelabel.grid(row=2, column=1, sticky="w")
-radiobutton1 = Radiobutton(tab1, text="Human", variable=p1, value="Human", command=lambda: set_player_type(0))
+
+radiobutton1 = ttk.Radiobutton(tab1, text="Human", variable=p1, value="Human", command=lambda: set_player_type(0),style="TRadiobutton")
 radiobutton1.grid(row=3, column=0, sticky="w")
-radiobutton2 = Radiobutton(tab1, text="TestAI", variable=p1, value="AI", command=lambda: set_player_type(0))
+radiobutton2 = ttk.Radiobutton(tab1, text="TestAI", variable=p1, value="AI", command=lambda: set_player_type(0),style="TRadiobutton")
 radiobutton2.grid(row=4, column=0, sticky="w")
-radiobutton3 = Radiobutton(tab1, text="MM-AI", variable=p1, value="MM-AI", command=lambda: set_player_type(0))
+radiobutton3 = ttk.Radiobutton(tab1, text="MM-AI", variable=p1, value="MM-AI", command=lambda: set_player_type(0),style="TRadiobutton")
 radiobutton3.grid(row=5, column=0, sticky="w")
-radiobutton4 = Radiobutton(tab1, text="Human", variable=p2, value="Human", command=lambda: set_player_type(1))
+radiobutton4 = ttk.Radiobutton(tab1, text="Human", variable=p2, value="Human", command=lambda: set_player_type(1),style="TRadiobutton")
 radiobutton4.grid(row=3, column=1, sticky="w")
-radiobutton5 = Radiobutton(tab1, text="TestAI", variable=p2, value="AI", command=lambda: set_player_type(1))
+radiobutton5 = ttk.Radiobutton(tab1, text="TestAI", variable=p2, value="AI", command=lambda: set_player_type(1),style="TRadiobutton")
 radiobutton5.grid(row=4, column=1, sticky="w")
-radiobutton6 = Radiobutton(tab1, text="MM-AI", variable=p2, value="MM-AI", command=lambda: set_player_type(1))
+radiobutton6 = ttk.Radiobutton(tab1, text="MM-AI", variable=p2, value="MM-AI", command=lambda: set_player_type(1),style="TRadiobutton")
 radiobutton6.grid(row=5, column=1, sticky="w")
-gamerunslabel = Label(tab1, text="Number of games: ", font=(style_numbers[0], style_numbers[1]))
+
+gamerunslabel = ttk.Label(tab1, text="Number of games: ",style="TLabel")
 gamerunslabel.grid(row=6, column=0, sticky="w")
-gamerunsentry = ttk.Entry(tab1, textvariable=game_runs)
+gamerunsentry = ttk.Entry(tab1, textvariable=game_runs,style="TEntry")
 gamerunsentry.grid(row=6, column=1, sticky="w")
-delaybutton = Checkbutton(tab1, text="Use AI Delay", variable=delayvar, font=(style_numbers[0], style_numbers[1]))
+
+delaybutton = ttk.Checkbutton(tab1, text="Use AI Delay", variable=delayvar,style="TCheckbutton")
 delaybutton.grid(row=7, column=0, sticky="w")
-logbutton = Checkbutton(tab1, text="Create log file", variable=logvar, font=(style_numbers[0], style_numbers[1]))
+logbutton = ttk.Checkbutton(tab1, text="Create log file", variable=logvar,style="TCheckbutton") 
 logbutton.grid(row=8, column=0, sticky="w")
-replaybutton = Checkbutton(tab1, text="Save replays", variable=repvar, font=(style_numbers[0], style_numbers[1]))
+replaybutton = ttk.Checkbutton(tab1, text="Save replays", variable=repvar,style="TCheckbutton") 
 replaybutton.grid(row=9, column=0, sticky="w")
-button_3 = Button(input_canvas, text="Quit Game", bg=style_numbers[2], font=(style_numbers[0], style_numbers[1]), width=style_numbers[3], height=style_numbers[4], command=lambda: quit_game())
+button_3 = ttk.Button(input_canvas, text="Quit Game", style="TButton", command=lambda: quit_game())
 button_3.grid(row=1, column=0, sticky="e")
 
+
 ttk.Label(tab2)
-button_2 = Button(tab2, text="Train", bg=style_numbers[2], font=(style_numbers[0], style_numbers[1]), width=style_numbers[3], height=style_numbers[4], command=lambda: start_new_game(True))
+button_2 = ttk.Button(tab2, text="Train", style="TButton", command=lambda: start_new_game(True))
 button_2.grid(row=0, column=0, sticky="w")
-train_opponent_label = Label(tab2, text="Train against:", font=(style_numbers[0], style_numbers[1]))
+train_opponent_label = ttk.Label(tab2, text="Train against:", style="TLabel")
 train_opponent_label.grid(row=1, column=0, sticky="w")
-radiobutton7 = Radiobutton(tab2, text="MM-AI", variable=p2, value="MM-AI", command=lambda: set_player_type(1))
+radiobutton7 = ttk.Radiobutton(tab2, text="MM-AI", variable=p2, value="MM-AI", command=lambda: set_player_type(1),style="TRadiobutton")
 radiobutton7.grid(row=2, column=0, sticky="w")
-radiobutton8 = Radiobutton(tab2, text="TestAI", variable=p2, value="AI", command=lambda: set_player_type(1))
+radiobutton8 = ttk.Radiobutton(tab2, text="TestAI", variable=p2, value="AI", command=lambda: set_player_type(1),style="TRadiobutton")
 radiobutton8.grid(row=3, column=0, sticky="w")
-gamerunslabel = Label(tab2, text="Number of games: ", font=(style_numbers[0], style_numbers[1]))
+gamerunslabel = ttk.Label(tab2, text="Number of games: ",style="TLabel")
 gamerunslabel.grid(row=4, column=0, sticky="w")
-gamerunsentry2 = Entry(tab2, textvariable=game_runs)
+gamerunsentry2 = ttk.Entry(tab2, textvariable=game_runs,style="TEntry")
 gamerunsentry2.grid(row=5, column=0, sticky="w")
-replaybutton2 = Checkbutton(tab2, text="Save replays", variable=repvar, font=(style_numbers[0], style_numbers[1]))
+replaybutton2 = ttk.Checkbutton(tab2, text="Save replays", variable=repvar,style="TCheckbutton")
 replaybutton2.grid(row=6, column=0, sticky="w")
 train_description = Label(tab2, text="It is recommended to run at least 3 000 games per training session.", font=(style_numbers[0], style_numbers[1]), wraplength=WIDTH-5, justify=LEFT)
 train_description.grid(row=7, column=0, sticky="w")
 
+
 ttk.Label(tab3)
-replaylabel = Label(tab3, text="Choose the replay file: ", font=(style_numbers[0], style_numbers[1]))
+replaylabel = ttk.Label(tab3, text="Choose the replay file: ",style="TLabel")
 replaylabel.grid(row=0, column=0, sticky="w")
-replayentry = Entry(tab3, textvariable=replay_path, width=30)
+replayentry = ttk.Entry(tab3, textvariable=replay_path, width=30,style="TEntry")
 replayentry.grid(row=1, column=0, sticky="w")
-button_4 = Button(tab3, text="...", bg=style_numbers[2], font=(style_numbers[0], style_numbers[1]), command=lambda: browse_files())
+button_4 = ttk.Button(tab3, text="...",style="TButton", command=lambda: browse_files())
 button_4.grid(row=1, column=1, sticky="w")
-delaybutton2 = Checkbutton(tab3, text="Use AI Delay", variable=delayvar, font=(style_numbers[0], style_numbers[1]))
+delaybutton2 = ttk.Checkbutton(tab3, text="Use AI Delay", variable=delayvar, style="TCheckbutton")
 delaybutton2.grid(row=2, column=0, sticky="w")
-button_5 = Button(tab3, text="Play", bg=style_numbers[2], font=(style_numbers[0], style_numbers[1]), width=style_numbers[3], height=style_numbers[4], command=lambda: replay())
+button_5 = ttk.Button(tab3, text="Play", style="TButton", command=lambda: replay())
 button_5.grid(row=3, column=0, sticky="w")
 
 def mainmenu_run():
