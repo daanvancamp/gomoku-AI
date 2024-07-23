d35ab470ff5878922a714b3b363f672f5ffca046
daanvancamp <163452511+daanvancamp@users.noreply.github.com>
2024-07-23 10:33:54 +0200

    code cleanup

--- filereader.py
+++ filereader.py
@@ -5,7 +5,6 @@ import os
 def log_info_overruling(message):
     with open("logging_overruling.txt", "a") as file:
         file.write(message)
-
 def create_gomoku_game(filename):
     with open(filename, 'r') as file:
         data = json.load(file)
