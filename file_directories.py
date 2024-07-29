1ac197cd32ed468273c616128b241af208886304
daanvancamp <163452511+daanvancamp@users.noreply.github.com>
2024-07-29 16:16:19 +0200

    moved a few files

---
+++ file_directories.py
@@ -0,0 +1,6 @@
+path_last_active_tab=r".\configs\last_active_tab.txt"
+path_models_players=r".\configs\models_players.txt"
+path_vars=r".\configs\vars.txt"
+path_logging_overruling=r".\logging_overruling.txt"
+path_wachten_muziek=r".\wachten_muziek.mp3"
+#now you can easily change all paths in the code
